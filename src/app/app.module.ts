b2e9c22c9b4836bfdd05de223a95212fa074c853
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-02-28 18:05:33 -0600

    Generated 3 new components...
    ng generate component home --skip-tests
    
    ng generate component setup-game --skip-tests
    
    ng generate component play-game --skip-testsng generate component play-game --skip-tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { SetupGameComponent } from './setup-game/setup-game.component';
+import { PlayGameComponent } from './play-game/play-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SetupGameComponent,
+    PlayGameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/setup-game/setup-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setup-game',
+  templateUrl: './setup-game.component.html',
+  styleUrls: ['./setup-game.component.css']
+})
+export class SetupGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
