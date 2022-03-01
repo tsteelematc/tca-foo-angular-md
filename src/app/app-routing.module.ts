84adf9eaaa85862293a1bc9a1514cc20a8e370bd
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-02-28 18:11:30 -0600

    Hooked up routing and got rid of Angular new app boilerplate...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlayGameComponent } from './play-game/play-game.component';
+import { SetupGameComponent } from './setup-game/setup-game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent}
+  , { path: "setup", component: SetupGameComponent}
+  , { path: "play", component: PlayGameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
