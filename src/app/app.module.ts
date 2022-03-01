9c7d53377afcd5d69ff2b119b9010c701cb0ad92
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-02-28 18:17:35 -0600

    mat-button working...
    Next try navigation ? ? ?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { SetupGameComponent } from './setup-game/setup-game.component';
 import { PlayGameComponent } from './play-game/play-game.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { PlayGameComponent } from './play-game/play-game.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
